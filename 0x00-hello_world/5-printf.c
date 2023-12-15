e429ce24637a24a799addf92aaaeaa2bc3d6a2bd
ahmed <a7medababss@gmail.com>
2023-12-14 16:20:43 -0800

    agian

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the starting point
+ *
+ * Return: always 0 when succeed
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
