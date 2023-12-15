7476da17269291912f00bd572053a3d1d1830336
ahmed <a7medababss@gmail.com>
2023-12-15 14:56:45 -0800

    reverse order of alphabt

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of program
+ *
+ * Returb: always 0 when succeed
+ */
+
+int main(void)
+{
+	int i;
+	char c = 'z';
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
