8d1d17c460b0340ff765c5ee92f40f204e9b3a6e
ahmed <a7medababss@gmail.com>
2023-12-22 11:06:23 -0800

    numbers from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints from 0 to 14
+ *
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j < 10)
+			{
+				_putchar('0' + j);
+			}
+			else
+			{
+
+				_putchar('0' + j / 10);
+				_putchar('0' + j % 10);
+			}
+		}
+		_putchar('\n');
+	}
+}
