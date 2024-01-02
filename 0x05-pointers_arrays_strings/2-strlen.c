b135c9a9eb258527d089850ff427450d6cc7f5f6
ahmed <a7medababss@gmail.com>
2024-01-02 12:02:54 -0800

    ..

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - pass size of string
+ *
+ * @s: passed string
+ *
+ * Return: always size of string
+ */
+
+int _strlen(char *s)
+{
+	int _size;
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_size = sizeof(s[i]) * (i + 1);
+	}
+	return (_size);
+}
