730080c1389926137cfee099ced5cfb32e81e124
ahmed <a7medababss@gmail.com>
2024-01-02 17:01:53 -0800

    ..

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - it reverse string without printing
+ *
+ * the main function will print the reversed string;
+ *
+ * @s: passed string
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[j] != 0)
+	{
+		j++;
+	}
+	j--;
+	while (j > i)
+	{
+		char temp;
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	}
+}
+
+
