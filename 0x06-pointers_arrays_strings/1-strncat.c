4577346014ac969622f984e6810d6f75a739bff6
ahmed <a7medababss@gmail.com>
2024-01-04 07:54:32 -0800

    ..

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - it will concatenate two strings
+ *
+ * @dest: the first passed string
+ *
+ * @src: the second one
+ *
+ * @n: the number needed to pass
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0' && j <= n - 1)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
