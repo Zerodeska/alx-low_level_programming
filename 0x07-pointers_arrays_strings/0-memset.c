927b561e3e9b581e3edfc80dc223c926cb69839c
ahmed <a7medababss@gmail.com>
2024-01-08 06:49:58 -0800

    ..

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,16 @@
 #include "main.h"
 
-
+/**
+ * _memset - it fills memory with one byte
+ *
+ * @s: passed string
+ *
+ * @b: passed byte
+ *
+ * @n: number of elements to change
+ *
+ * Return: the modified string
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -11,5 +21,5 @@ char *_memset(char *s, char b, unsigned int n)
 		s[i] = b;
 		i++;
 	}
-	return s;
+	return (s);
 }
