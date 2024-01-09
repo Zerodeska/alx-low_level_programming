42d0030723f5045f5db9cc9bf6a401f5ae338cb3
ahmed <a7medababss@gmail.com>
2024-01-09 04:52:58 -0800

    ..

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,14 +13,27 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i = 0;
+	int n = 0;
+	unsigned int length = 0;
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == accept[0])
+		n = 0;
+
+		while (accept[n] != '\0')
 		{
-			return (i + 1);
+			if (s[i] == accept[n])
+			{
+				length++;
+				break;
+			}
+			n++;
+		}
+		if (accept[n] == '\0')
+		{
+			break;
 		}
 		i++;
 	}
-	return ('\0');
+	return (length);
 }
