94efc69c763256da3be3ac0b2863b52929cb6955
ahmed <a7medababss@gmail.com>
2024-01-11 10:28:30 -0800

    ..

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+int _test(int c, int v);
+
+/**
+ * is_prime_number - it will test the number
+ *
+ * @n: passed number
+ *
+ * Return: the result 0 or 1
+ */
+
+int is_prime_number(int n)
+{
+	int x;
+
+	if (n == 0 || n == 1 || n == -1)
+		return (0);
+	x = _test(n, 2);
+	return (x);
+}
+/**
+ * _test - testing process
+ *
+ * @c: passed number
+ * @v: used to test
+ *
+ * Return: 0 or 1
+ */
+int _test(int c, int v)
+{
+	if (c % v == 0)
+		return (0);
+	if (v > c / 2)
+		return (1);
+	return (_test(c, v + 1));
+}
