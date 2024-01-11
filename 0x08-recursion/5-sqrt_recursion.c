517dc6e8f291cefecab192d9c54d051a02e441e5
ahmed <a7medababss@gmail.com>
2024-01-11 09:33:00 -0800

    ..

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+int _test(int c, int v);
+/**
+ * _sqrt_recursion _ - returns the sqrt
+ *
+ * @n: passed number
+ *
+ * Return: the result
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+	{
+		int x = _test(n, 0);
+		return x;
+	}
+}
+/**
+ * _test - determines the sqr
+ *
+ * @c: the passed argument
+ *
+ * @v: tested number
+ *
+ * Return: the value
+ */
+int _test(int c, int v)
+{
+	if (v * v == c)
+		return (v);
+	else if (v * v != c && v < c)
+		return _test(c, v + 1);
+	else
+		return (-1);
+}
