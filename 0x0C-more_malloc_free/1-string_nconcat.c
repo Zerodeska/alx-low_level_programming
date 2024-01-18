1d3eb25bf15adb3022797bba4ceedd807753b2b3
ahmed <a7medababss@gmail.com>
2024-01-18 11:03:18 -0800

    ..

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,6 +5,7 @@
  *
  * @s1: first passed string
  * @s2: second passed string
+ * @n: number of elements in second stirng
  *
  * Return: the string
  */
@@ -15,6 +16,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned long int i = 0;
 	unsigned int j = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	p = malloc(strlen(s1) + n + 1);
 	if (p == NULL)
 	{
