99bad71811ceabbb617455a518506b527a1e9c49
ahmed <a7medababss@gmail.com>
2024-01-22 11:24:43 -0800

    ..

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog 
+/**
+ * struct dog - contains dog's info
+ *
+ * @name: first element
+ * @owner: second element
+ * @age: third element
+ *
+ * Description: longer description
+ */
+
+struct dog
 {
 	char *name;
 	char *owner;
