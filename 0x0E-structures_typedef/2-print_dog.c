65701b17a45433c4a42b100c44a300cbc4fb08f9
ahmed <a7medababss@gmail.com>
2024-01-23 01:01:14 -0800

    ..

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints dog's elements
+ *
+ * @d: passed structure
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		d->name = "nill";
+	if (d->owner == NULL)
+		d->owner = "nill";
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - contains dog's info
  *
@@ -19,5 +22,6 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
