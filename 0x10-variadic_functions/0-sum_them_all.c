1a7c693559df1f0f5cc4f48e9d5346e6c42cde44
ahmed <zero@gmail.com>
2024-01-27 09:29:09 +0200

    ..

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - it will make addition for every passed number
+ * @n: passed  number of arguments
+ * Return: the result
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+va_list a;
+unsigned i;
+int sum = 0;
+
+if (n == 0)
+return (0);
+va_start(a, n);
+for (i = 0; i < a; i++)
+{
+int x = va_arg(a, int);
+
+sum += x;
+}
+return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+here is variadic function
