1471c8e6a91d33503f1cc784b828a7b03a573321
zerodeska <a7medabass@gmail.com>
2024-02-10 22:08:49 +0200

    ..

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * set_bit - it sets index's bet into 1
+ * @n: passed number
+ * @index: passed index
+ * Return: 1 or -1
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int long power = 1UL << index;
+
+	*n |= power;
+	return (1);
+}
