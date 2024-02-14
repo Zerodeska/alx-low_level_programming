42fa27bdc367f965d645a5a0fba3b4675bf3856e
zerodeska <a7medabass@gmail.com>
2024-02-14 13:18:36 +0200

    ..

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,19 +2,17 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fp, b;
-	char *buffer;
+	int fp;
+	size_t b, a;
+	char buffer[1024];
 
 	if (filename == NULL || letters == 0)
 		return (0);
 	fp = open(filename, O_RDONLY);
 	if (fp == -1)
 		return (0);
-	buffer = malloc((letters + 1) * sizeof(char));
-	if (buffer == NULL)
-		return (0);
-	b = read(fp, &buffer, letters);
-	b = write(STDOUT_FILENO, &buffer, b);
+	b = read(fp, buffer, letters);
+	a = write(STDOUT_FILENO, buffer, b);
 	close(fp);
-	return (b);
+	return (a);
 }
