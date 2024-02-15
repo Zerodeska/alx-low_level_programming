6fc7dfa621cf984fef32254bab27c6a50f43cee7
zerodeska <a7medabass@gmail.com>
2024-02-15 10:26:55 +0200

    ..

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fp, wr, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	while (text_content[i] != '\0')
+		i++;
+	fp = open(filename, O_WRONLY | O_APPEND);
+	if (fp == -1)
+		return (-1);
+	wr = write(fp, text_content, i);
+	if (wr == -1)
+	{
+		return (-1);
+		close(fp);
+	}
+	close(fp);
+	return (1);
+}
