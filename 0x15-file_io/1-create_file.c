05712ca0cb105f7ca70dabe512ec27362a97ef62
zerodeska <a7medabass@gmail.com>
2024-02-15 10:10:29 +0200

    ..

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,6 +11,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (op == -1)
 		return (-1);
@@ -19,5 +21,6 @@ int create_file(const char *filename, char *text_content)
 	wr = write(op, text_content, i);
 	if (wr == -1)
 		return (-1);
+	close(op);
 	return (1);
 }
